88cd14c4630a5f1b385b095b3bedfb34496ede35
冯 <2789083329@qq.com>
2024-11-01 09:58:04 +0800

    为拦截体执行后的回调行为新增一个参数用于接受上一步骤的返回值

--- AOP/ProxyInstance.cs
+++ AOP/ProxyInstance.cs
@@ -13,12 +13,14 @@ namespace MinimalisticWPF
     /// 编写 [ 切面逻辑 ] 时使用
     /// </summary>
     /// <param name="args">在覆写方法时,由args顺序获取本次传给方法的参数值</param>
+    /// <param name="lastResult">上一个方法返回的值</param>
+    /// <param name="result">方法执行结束后,由result获取本次方法返回值</param>
     /// <returns>
     /// <para>[ getter ] 覆写时 , 需要返回新的getter逻辑返回的值</para>
     /// <para>[ method ] 覆写时 , 需要返回新的method逻辑返回的值</para>
     /// <para>[ 扩展 ] 而非覆写时,返回null即可</para>
     /// </returns>
-    public delegate object? ProxyHandler(object?[]? args);
+    public delegate object? ProxyHandler(object?[]? args, object? lastResult);
 
     /// <summary>
     /// 代理中间件,存储代理行为
@@ -48,26 +50,26 @@ namespace MinimalisticWPF
             if (Name.StartsWith("get_"))
             {
                 GetterActions.TryGetValue(Name, out var actions);
-                actions?.Item1?.Invoke(args);
-                var result = actions?.Item2 == null ? _targetType?.GetMethod(Name)?.Invoke(_target, args) : actions.Item2.Invoke(args);
-                actions?.Item3?.Invoke(args);
-                return result;
+                var R0 = actions?.Item1?.Invoke(args, null);
+                var R1 = actions?.Item2 == null ? _targetType?.GetMethod(Name)?.Invoke(_target, args) : actions.Item2.Invoke(args, R0);
+                actions?.Item3?.Invoke(args, R1);
+                return R1;
             }
             else if (Name.StartsWith("set_"))
             {
                 SetterActions.TryGetValue(Name, out var actions);
-                actions?.Item1?.Invoke(args);
-                var result = actions?.Item2 == null ? _targetType?.GetMethod(Name)?.Invoke(_target, args) : null;
-                actions?.Item3?.Invoke(args);
-                return result;
+                var R0 = actions?.Item1?.Invoke(args, null);
+                var R1 = actions?.Item2 == null ? _targetType?.GetMethod(Name)?.Invoke(_target, args) : actions.Item2.Invoke(args, R0);
+                actions?.Item3?.Invoke(args, R1);
+                return R1;
             }
             else
             {
                 MethodActions.TryGetValue(Name, out var actions);
-                actions?.Item1?.Invoke(args);
-                var result = actions?.Item2 == null ? _targetType?.GetMethod(Name)?.Invoke(_target, args) : actions.Item2.Invoke(args);
-                actions?.Item3?.Invoke(args);
-                return result;
+                var R0 = actions?.Item1?.Invoke(args, null);
+                var R1 = actions?.Item2 == null ? _targetType?.GetMethod(Name)?.Invoke(_target, args) : actions.Item2.Invoke(args, R0);
+                actions?.Item3?.Invoke(args, R1);
+                return R1;
             }
         }
     }
